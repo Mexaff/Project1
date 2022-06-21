8ae69cb8d73bad2c7507476035882058edd39838
Денис Гордіюк <denis30042002@gmail.com>
2022-06-21 12:41:19 +0300

    1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,4 +19,12 @@ class AdminController extends Controller
         return redirect()->route('login');
     }
 
+
+    public function profile()
+    {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+
+        return view('admin.admin_profile_view', compact('adminData'));
+    }
 }
